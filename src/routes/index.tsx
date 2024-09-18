9f737ed90c073e73847f7dc11ec1e3472ce5b648
Jakob Norlin <jakob.stahl91@gmail.com>
2024-09-18 23:08:11 +0200

    Reproduce twitchy CSS

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createAsync } from "@solidjs/router"
+
+export default function Home() {
+	const data = createAsync(() => Promise.resolve("data"))
+
+	return (
+		<main class="main">
+			{data()}
+
+			<div class="square solid">solid</div>
+			<div class="square twitchy">twitchy</div>
+		</main>
+	)
+}
